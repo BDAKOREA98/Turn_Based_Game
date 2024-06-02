48934c0f29c22fb5a7f6ef9d75ed7455d4ba7c03
BDAKOREA98 <nani1541@gmail.com>
2024-06-02 23:19:43 +0900

    06.02
    Flip

--- Assets/Scripts/Scripts/Movement/Move.cs
+++ Assets/Scripts/Scripts/Movement/Move.cs
@@ -12,13 +12,16 @@ public class Move : MonoBehaviour
     private int currentStep;
     Vector3 targetPos;
     float speedOfAnim = 2f;
+    
+    internal bool lookingToTheRight = true;
+    SpriteRenderer heroSprite;
 
 
 
     private void Start()
     {
         hero = GetComponent<Hero>();
-       
+        heroSprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -45,11 +48,15 @@ public class Move : MonoBehaviour
         targetPos = new Vector3(path[currentStep].transform.position.x,
             path[currentStep].transform.position.y,
             transform.position.z);
-
+        ControlDirection(targetPos);
     }
     private void HeroIsMoving()
     {
 
+        if (targetPos == new Vector3(0,0,0))
+        {
+            return;
+        }
 
         transform.position = Vector3.MoveTowards(transform.position, targetPos,
                              speedOfAnim * Time.deltaTime);
@@ -80,7 +87,17 @@ public class Move : MonoBehaviour
         isMoving = !isMoving;
         hero.GetComponent<Animator>().SetBool("IsMoving", false);
     }
-
+    internal void ControlDirection(Vector3 targetPos)
+    {
+        //compares the coordinates of the hero and the coordinates of the target hex
+        //rotates the hero if necessary
+        if (transform.position.x > targetPos.x && lookingToTheRight ||
+            transform.position.x < targetPos.x && !lookingToTheRight)
+        {
+            heroSprite.flipX = !heroSprite.flipX;//rotates a sprite of the hero
+            lookingToTheRight = !lookingToTheRight;//sets the opposite value for a variable
+        }
+    }
 
 
 }
